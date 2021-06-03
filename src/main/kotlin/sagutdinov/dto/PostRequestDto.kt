c5db5cde7ddda8d64a8ef299c4bf48ebfd550401
Azatka06 <57129626+Azatka06@users.noreply.github.com>
2021-06-03 15:02:33 +0300

    new

--- src/main/kotlin/sagutdinov/dto/PostRequestDto.kt
+++ src/main/kotlin/sagutdinov/dto/PostRequestDto.kt
@@ -4,6 +4,6 @@ package ru.sagutdinov.dto
 data class PostRequestDto (
     val postName: String? = null,
     val postText: String? = null,
-    val attachmentImage: String,
-    val link: String? = null
+    val link: String? = null,
+    val attachmentImage: String
 )
